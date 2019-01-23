ef1dc91896d5f58462b5c1f4c37f1ff4777b8d8d
Michael Gibson Bethke <gibsonbethke@gmail.com>
2019-01-23 11:59:05 -0600

    working on proj 3

--- AssignmentOne/Simple_Calculator/src/main.cc
+++ AssignmentOne/Simple_Calculator/src/main.cc
@@ -1,11 +1,67 @@
 #include <iostream>
 
 
-class fraction {
-public:
+struct fraction {
 	int numerator;
 	int denominator;
 };
+
+
+fraction result (const fraction *fraction_one, const char *op, const fraction *fraction_two)
+{
+	
+	fraction fraction_result;
+	
+	switch (*op)
+	{
+		
+		case '+':
+		break;
+		
+		case '-':
+		break;
+		
+		case '*':
+		case 'x':
+		break;
+		
+		case '/':
+		break;
+		
+		default:
+		break;
+	}
+	
+	return fraction_result;
+}
+
+
+void output (const fraction *fraction_one, const char *op, const fraction *fraction_two)
+{
+	
+	
+	
+	std::cout << "" << std::endl;
+}
+
+
+void prompt_for_input (fraction *fract, char *op = nullptr)
+{
+	
+	if (fract != nullptr)
+	{
+		
+		char slash;
+	
+		std::cout << "Enter a fraction (match the pattern x/y): ";
+		std::cin >> fract->numerator >> slash >> fract->denominator;
+	} else if (op != nullptr)
+	{
+		
+		std::cout << "Enter an operation (+ - * /): ";
+		std::cin >> op;
+	}
+}
 
 
 int main (int argc, char const *argv[])
@@ -15,12 +71,23 @@ int main (int argc, char const *argv[])
 	
 	fraction fraction_one;
 	fraction fraction_two;
+	char operation;
+	
+	char again;
 	
 	do
 	{
 		
+		prompt_for_input (&fraction_one);
+		prompt_for_input (nullptr, &operation);
+		prompt_for_input (&fraction_two);
 		
-	} while (true);
+		output (&fraction_one, &operation, &fraction_two);
+		
+		std::cout << "Continue? (y/n) ";
+		std::cin >> again;
+		
+	} while (again != 'y' || again != 'Y');
 	
 	return 0;
 }
