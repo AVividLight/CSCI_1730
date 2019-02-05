7b8f3816833fd67aafd7d8eb4c219de8c44e29f7
Michael Gibson Bethke <gibsonbethke@gmail.com>
2019-02-04 19:26:30 -0600

    working on geometry calculations

--- AssignmentTwo/Geometry_Calculations/src/main.cc
+++ AssignmentTwo/Geometry_Calculations/src/main.cc
@@ -1,8 +1,24 @@
 #include <iostream>
 
 
+const char *ascii_circle = "    x  x\n x        x\nx          x\nx          x\n x        x\n    x  x";
+const char *ascii_rectangle = "";
+const char *ascii_triangle = "";
+
+
+void get_dimensions ()
+{
+	
+	std::cout << "Enter ";
+}
+
+
 int main (int argc, char const *argv[])
 {
-	/* code */
+	
+	std::cout << std::endl << "Project 1 from Assignment 2" << std::endl << "Geometry Calculations" << std::endl << std::endl << "This program will prompt for a shape and dimensions, then return its area and perimeter." << std::endl << std::endl;
+	
+	std::cout << ascii_circle << std::endl;
+	
 	return 0;
 }
