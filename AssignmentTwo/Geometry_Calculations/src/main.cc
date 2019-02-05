0b66803fd2b48f13232a6441dc4a52898806c610
Michael Bethke <gibsonbethke@gmail.com>
2019-02-05 00:13:58 -0600

    made some ascii art

--- AssignmentTwo/Geometry_Calculations/src/main.cc
+++ AssignmentTwo/Geometry_Calculations/src/main.cc
@@ -2,8 +2,8 @@
 
 
 const char *ascii_circle = "    x  x\n x        x\nx          x\nx          x\n x        x\n    x  x";
-const char *ascii_rectangle = "";
-const char *ascii_triangle = "";
+const char *ascii_rectangle = "+---------+\n|         |\n|         |\n|         |\n|         |\n+---------+";
+const char *ascii_triangle = "     /\\\n    /  \\\n   /    \\\n  /      \\\n /        \\\n/__________\\";
 
 
 void get_dimensions ()
@@ -18,7 +18,11 @@ int main (int argc, char const *argv[])
 	
 	std::cout << std::endl << "Project 1 from Assignment 2" << std::endl << "Geometry Calculations" << std::endl << std::endl << "This program will prompt for a shape and dimensions, then return its area and perimeter." << std::endl << std::endl;
 	
-	std::cout << ascii_circle << std::endl;
+	std::cout << ascii_circle << std::endl << "(c) Circle" << std::endl;
+	
+	std::cout << ascii_rectangle << std::endl << "(r) Rectangle" << std::endl;
+	
+	std::cout << ascii_triangle << std::endl << "(t) Triangle" << std::endl;
 	
 	return 0;
 }
