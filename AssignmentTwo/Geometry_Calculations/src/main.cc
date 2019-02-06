26f2fc9b01c1f848569ef8ad7ae528347df43cd2
Michael Gibson Bethke <gibsonbethke@gmail.com>
2019-02-06 11:15:18 -0600

    Merging ascii art

--- AssignmentTwo/Geometry_Calculations/src/main.cc
+++ AssignmentTwo/Geometry_Calculations/src/main.cc
@@ -1,9 +1,12 @@
 #include <iostream>
 
 
-const char *ascii_circle = "    x  x\n x        x\nx          x\nx          x\n x        x\n    x  x";
-const char *ascii_rectangle = "+---------+\n|         |\n|         |\n|         |\n|         |\n+---------+";
-const char *ascii_triangle = "     /\\\n    /  \\\n   /    \\\n  /      \\\n /        \\\n/__________\\";
+//const char *ascii_circle = "    x  x\n x        x\nx          x\nx          x\n x        x\n    x  x";
+//const char *ascii_rectangle = "+---------+\n|         |\n|         |\n|         |\n|         |\n+---------+";
+//const char *ascii_triangle = "     /\\\n    /  \\\n   /    \\\n  /      \\\n /        \\\n/__________\\";
+
+const char *ascii_menu = "    x  x         +---------+  	     /\\\n x        x      |         |  	    /  \\\nx          x	 |         |  	   /    \\\nx          x     |         |  	  /      \\\n x        x      |         |  	 /        \\\n    x  x         +---------+  	/__________\\\n (c) Circle	(r) Rectangle    (t) Triangle";
+const float pi = 3.141592654;
 
 
 void get_dimensions ()
@@ -18,11 +21,35 @@ int main (int argc, char const *argv[])
 	
 	std::cout << std::endl << "Project 1 from Assignment 2" << std::endl << "Geometry Calculations" << std::endl << std::endl << "This program will prompt for a shape and dimensions, then return its area and perimeter." << std::endl << std::endl;
 	
-	std::cout << ascii_circle << std::endl << "(c) Circle" << std::endl;
+	char menu;
 	
-	std::cout << ascii_rectangle << std::endl << "(r) Rectangle" << std::endl;
-	
-	std::cout << ascii_triangle << std::endl << "(t) Triangle" << std::endl;
+	do {
+		std::cout << ascii_menu << std::endl << std::endl;
+		std::cout << " (e) Exit" << std::endl;
+		
+		std::cin >> menu;
+		menu = tolower (menu);
+		
+		switch (menu)
+		{
+			
+			case 'c':
+			get_dimensions ();
+			break;
+			
+			case 'r':
+			get_dimensions ();
+			break;
+			
+			case 't':
+			get_dimensions ();
+			break;
+			
+			default:
+			break;
+		}
+		
+	} while (menu != 'e');
 	
 	return 0;
 }
