cdcc4eabce6141ffa307e53a20dc781b46f17bfa
Michael Gibson Bethke <gibsonbethke@gmail.com>
2019-02-20 11:24:35 -0600

    Started point structure

---
+++ AssignmentTwo/Point_Structure/src/main.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+
+int main (int argc, char const *argv[])
+{
+	
+	std::cout << std::endl << "Project 5 from Assignment 2" << std::endl << "Point Structure" << std::endl << std::endl << "This program will prompt for a number of dice-throwing simulations, then calculate and display the total number of each sum produced along with its percentage of the total tosses." << std::endl << std::endl;
+	
+	return 0;
+}
