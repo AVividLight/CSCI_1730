2a273fba2074f2360e9c7b434933b02989ff1738
Michael Bethke <gibsonbethke@gmail.com>
2019-04-23 23:18:27 -0500

    Added shape maths

--- AssignmentFour/Pure_Virtual_Shapes/readme.md
+++ AssignmentFour/Pure_Virtual_Shapes/readme.md
@@ -1,4 +1,4 @@
-Incomplete as of 3/28/2019
+Incomplete as of 4/23/2019
 ===
 
 Write a C++ program that uses virtual functions to input, calculate, and display some quantities associated with a variety of 2- and 3-dimensional objects. Design an abstract base class `Shape` from which you will derive classes `Rectangle`, `Circle`, `Triangle`, `Box`, `Can`, `Cone`, and `Ball`. The class `Shape` should contain `pure virtual functions` `Display`, `GetDimensions`, `Area`, `Perimeter`, and `Volume`. For each of the derived classes, write `member functions` `Display`, `GetDimensions`, `Area`, `Perimeter`, and `Volume` that will do the following:
@@ -8,7 +8,7 @@ Write a C++ program that uses virtual functions to input, calculate, and display
 - `Perimeter` - Calculate and display the perimeter of an object of a 2-dimensional shape class; do nothing for an object of a 3-dimensional shape class.
 - `Area` - Calculate and display the area of an object of a 2-dimensional shape class; calculate and display the surface area for an object of a 3-dimensional shape class.
 - `Volume` - Calculate and display the volume of an object of a 3-dimensional shape class; do nothing for an object of a 2-dimensional shape class.
-- 
+
 Include appropriate constructors and any other member functions that you think are necessary. 
 
 Write a program driver that will allocate an array of 20 pointers to class `Shape` and then repeatedly prompt the user, up to a maximum 20 times, to enter a shape type, dynamically allocate memory for an appropriate shape object, and then prompt for and read the appropriate dimensions for the shape. After the input of shapes is complete, the program should then loop through the pointer array contents and display the shape type, dimensions, area, perimeter and/or volume for each of the input shape objects.
--- AssignmentFour/Pure_Virtual_Shapes/src/main.cc
+++ AssignmentFour/Pure_Virtual_Shapes/src/main.cc
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 
 
-class Shape
-{
+const double pi = 3.14159265358979323846;
 
+
+class Shape {
 public:
-	//Shape (std::size_t dim) {set_size (dim);}
 	Shape (std::size_t dim);
 	
 	virtual void display () = 0;
@@ -18,137 +19,146 @@ public:
 	void set_size (int dim) {size = dim;}
 	const std::size_t get_size () const {return size;}
 	
+	float get_dimension_at_index (const unsigned short int pos);
+	
 private:
 	float *dimension;
 	std::size_t size;
 };
 
 
-Shape::Shape (std::size_t dim)
-{
-	
+Shape::Shape (std::size_t dim) {
 	dimension = new float[dim];
 	size = dim;
 }
 
 
-
-class Rectangle : public Shape
-{
-
-public:
-	Rectangle ();
+float Shape::get_dimension_at_index (const unsigned short int pos) {
+	if (pos > size) {
+		throw;
+		return 0.0;
+	}
 	
-	void display ();
-    void GetDimensions ();
-    void Area ();
-    void Perimeter ();
-    void Volume ();
-	
-private:
-};
-
-
-Rectangle::Rectangle () : Shape (2)
-{
-	
-	
+	return dimension[pos];
 }
 
-/*class Circle : public Shape
-{
 
+
+class Rectangle : public Shape {
 public:
+	Rectangle () : Shape (SIDES) {}
 	
-	void display ();
-    void GetDimensions ();
-    void Area ();
-    void Perimeter ();
-    void Volume ();
+	void display () {std::cout << "Rectangle of length " << get_dimension_at_index (0) << " and width " << get_dimension_at_index (1) << std::endl;}
+    void GetDimensions () {}
+    void Area () {std::cout << "Area: " << (pi * get_dimension_at_index (0) * get_dimension_at_index (0)) << std::endl;}
+    void Perimeter () {std::cout << "Perimeter: " << (2 * pi * get_dimension_at_index (0)) << std::endl;}
+    void Volume () {} //Do nothing for 2D shape
 	
 private:
+	static const std::size_t SIDES = 2;
 };
 
-class Triangle : public Shape
-{
-	
-public:
 
-	void display ();
-    void GetDimensions ();
-    void Area ();
-    void Perimeter ();
-    void Volume ();
+class Circle : public Shape {
+public:
+	Circle () : Shape (SIDES) {}
+	
+	void display () {std::cout << "Circle of radius " << get_dimension_at_index (0) << std::endl;}
+    void GetDimensions () {}
+    void Area () {std::cout << "Area: " << (get_dimension_at_index (0) * get_dimension_at_index (1)) << std::endl;}
+    void Perimeter () {std::cout << "Perimeter: " << (2 * get_dimension_at_index (0) + 2 * get_dimension_at_index (1)) << std::endl;}
+    void Volume () {} //Do nothing for 2D shape
 	
 private:
+	static const std::size_t SIDES = 1;
 };
 
-class Box : public Shape
-{
-	
-public:
 
-	void display ();
-    void GetDimensions ();
+class Triangle : public Shape {
+public:
+	Triangle () : Shape (SIDES) {}
+	
+	void display () {std::cout << "Triangle of size " << get_dimension_at_index (0) << ", " << get_dimension_at_index (1) << ", and " << get_dimension_at_index (2) << std::endl;}
+    void GetDimensions () {}
     void Area ();
-    void Perimeter ();
-    void Volume ();
+    void Perimeter () {std::cout << "Perimeter: " << (get_dimension_at_index (0) + get_dimension_at_index (1) + get_dimension_at_index (2)) << std::endl;}
+    void Volume () {} //Do nothing for 2D shape
 	
 private:
+	static const std::size_t SIDES = 3;
 };
 
-class Can : public Shape
-{
-	
-public:
 
-	void display ();
-    void GetDimensions ();
-    void Area ();
-    void Perimeter ();
-    void Volume ();
+void Triangle::Area () {
+	const float half_perimeter = (get_dimension_at_index (0) + get_dimension_at_index (1) + get_dimension_at_index (2))/2;
+	std::cout << "Area: " << sqrt (half_perimeter * (half_perimeter - get_dimension_at_index (0)) * (half_perimeter - get_dimension_at_index (1)) * (half_perimeter - get_dimension_at_index (2))) << std::endl;
+}
+
+
+class Box : public Shape {
+public:
+	Box () : Shape (SIDES) {}
+	
+	void display () {std::cout << "Box of size " << get_dimension_at_index (0) << ", " << get_dimension_at_index (1) << ", and " << get_dimension_at_index (2) << std::endl;}
+    void GetDimensions () {}
+    void Area () {std::cout << "Area: " << 2 * (get_dimension_at_index (0) * get_dimension_at_index (1) + get_dimension_at_index (0) * get_dimension_at_index (2) + get_dimension_at_index (1) * get_dimension_at_index (2)) << std::endl;}
+    void Perimeter (); //Do nothing for 3D shape
+    void Volume () {std::cout << "Volume: " << get_dimension_at_index (0) * get_dimension_at_index (1) * get_dimension_at_index (2) << std::endl;}
 	
 private:
+	static const std::size_t SIDES = 3;
 };
 
-class Cone : public Shape
-{
-	
+
+class Can : public Shape {
 public:
+	Can () : Shape (SIDES) {}
 	
-	void display ();
-    void GetDimensions ();
-    void Area ();
-    void Perimeter ();
-    void Volume ();
+	void display () {std::cout << "Can of radius " << get_dimension_at_index (0) << " and height" << get_dimension_at_index (1) << std::endl;}
+    void GetDimensions () {}
+    void Area () {std::cout << "Area: " << 2 * pi * (get_dimension_at_index (0) * get_dimension_at_index (0)) + 2 * pi * get_dimension_at_index (0) * get_dimension_at_index (1) << std::endl;}
+    void Perimeter (); //Do nothing for 3D shape
+    void Volume () {std::cout << "Volume: " << pi * get_dimension_at_index (0) * get_dimension_at_index (0) * get_dimension_at_index (1) << std::endl;}
 	
 private:
+	static const std::size_t SIDES = 2;
 };
 
-class Ball : public Shape
-{
-	
-public:
 
-	void display ();
-    void GetDimensions ();
-    void Area ();
-    void Perimeter ();
-    void Volume ();
+class Cone : public Shape {
+public:
+	Cone () : Shape (SIDES) {}
+	
+	void display () {std::cout << "Cone of radius " << get_dimension_at_index (0) << " and height" << get_dimension_at_index (1) << std::endl;}
+    void GetDimensions () {}
+    void Area () {std::cout << "Area: " << pi * (get_dimension_at_index (0) * get_dimension_at_index (0)) + pi * get_dimension_at_index (0) * sqrt (get_dimension_at_index (1) * get_dimension_at_index (1) + get_dimension_at_index (0) * get_dimension_at_index (0)) << std::endl;}
+    void Perimeter (); //Do nothing for 3D shape
+    void Volume () {std::cout << "Volume: " << 1.0/3.0 * pi * get_dimension_at_index (0) * get_dimension_at_index (0) * get_dimension_at_index (1) << std::endl;}
 	
 private:
+	static const std::size_t SIDES = 2;
 };
-*/
 
 
-const char get_input ()
-{
+class Ball : public Shape {
+public:
+	Ball () : Shape (SIDES) {}
+	
+	void display () {std::cout << "Ball of radius " << get_dimension_at_index (0) << std::endl;}
+    void GetDimensions () {}
+    void Area () {std::cout << "Area: " << 4 * pi * get_dimension_at_index (0) * get_dimension_at_index (0) << std::endl;}
+    void Perimeter (); //Do nothing for 3D shape
+    void Volume () {std::cout << "Volume: " << 4.0/3.0 * pi * get_dimension_at_index (0) * get_dimension_at_index (0) * get_dimension_at_index (0) << std::endl;}
+	
+private:
+	static const std::size_t SIDES = 1;
+};
 
+
+const char get_input () {
 	std::string input;
 	
-	do
-	{
-		
+	do {	
 		std::getline (std::cin, input);
 	} while (tolower (input[0]) != 'y' && tolower (input[0]) != 'n');
 	
@@ -156,18 +166,13 @@ const char get_input ()
 }
 
 
-bool another_shape ()
-{
-	
+bool another_shape () {
 	std::cout << std::endl << "Examine another file? (y/n): ";
 	return (get_input () == 'y' ? true : false);
 }
 
 
-int main (int argc, char const *argv[])
-{
-	
-	
+int main (int argc, char const *argv[]) {
 	std::cout << "Project 3 from Assignment 4" << std::endl << "Pure Virtual Shapes" << std::endl << "This program uses pure virtual functions to model primitive shapes." << std::endl << std::endl;
 	
 	//Shapes *shapes;
@@ -175,12 +180,9 @@ int main (int argc, char const *argv[])
 	
 	Rectangle rect;
 	
-	do
-	{
-		
+	do {
 		
 	} while (another_shape ());
-	
 	
 	return 0;
 }
