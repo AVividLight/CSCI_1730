00c981fb49247763a17a04cdb4ee1c99816760ef
Michael Gibson Bethke <gibsonbethke@gmail.com>
2019-05-01 11:05:24 -0500

    Added project for Link List

---
+++ AssignmentFive/Linked_List/src/main.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+
+
+int main (int argc, char const *argv[]) {
+	std::cout << "Project 2 from Assignment 5" << std::endl << "Link List" << std::endl << "This program implements a basic link list" << std::endl << std::endl;
+	
+	return 0;
+}
