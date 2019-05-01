ba8e2cb128b53a7411cb07ede1f7afe42e387a14
Michael Gibson Bethke <gibsonbethke@gmail.com>
2019-05-01 11:02:40 -0500

    Added project for Class Template

---
+++ AssignmentFive/Class_Template/src/main.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+
+
+int main (int argc, char const *argv[]) {
+	std::cout << "Project 1 from Assignment 5" << std::endl << "Class Template" << std::endl << "This program uses templates to model four different data types." << std::endl << std::endl;
+	
+	return 0;
+}
