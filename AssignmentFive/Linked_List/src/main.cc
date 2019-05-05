a508b5c23bee536b52a99b2e6eebc695e4e8b00f
Michael Bethke <gibsonbethke@gmail.com>
2019-05-05 14:30:04 -0500

    Removed templates for brevity

--- AssignmentFive/Linked_List/src/main.cc
+++ AssignmentFive/Linked_List/src/main.cc
@@ -1,93 +1,120 @@
 #include <iostream>
 
-#if defined __cpp_lib_concepts && __has_include(<type_traits>)
-#  include <type_traits> //-std=c++14
-#  define ENSURE_TYPE
-#endif
-
 #ifndef nullptr
 #define nullptr null
 #endif
 
 
-template <typename T>
-class LinkNode {
+class Node {
 public:
-	LinkNode () {link = nullptr; value = 0.0;}
-	LinkNode (LinkNode *l, T v) {link = l; value = v;}
-	
-	LinkNode *get_link () {return link;}
-	void set_link (LinkNode *node) {link = node;}
-	
-	T &get_value () {return &value;}
-	void set_value (T v) {value = v;}
+	Node () {link = nullptr; value = 0.0;}
+	Node (Node *l, float v) {link = l; value = v;}
 
+	Node *get_link () {return link;}
+	void set_link (Node *node) {link = node;}
+
+	const float get_value () const {return value;}
+	void set_value (float v) {value = v;}
+	
+	bool operator== (const Node *comp) {return (value == comp->get_value ());}
 private:
-	LinkNode *link;
-	T value;
+	Node *link;
+	float value;
 };
 
-
-template <typename T>
-#ifdef ENSURE_TYPE
-concept Arithmetic = requires (T type) { //-std=c++2a
-	std::is_arithmetic<type> == true;
-};
-template<Arithmetic T>
-#endif
 	
 class LinkedList {
 public:
-	LinkedList<T> ();
+	LinkedList ();
 	
-	LinkNode<T> *search (T *match, LinkNode<T> *start = head);
+	unsigned int search (const float match, Node *start);
 	
-	void insert_head (LinkNode<T> *node);
-	void insert (LinkNode<T> *node, LinkNode<T> *after);
+	void insert_head (Node *node);
+	void insert (Node *node, Node *after);
 	
-	LinkNode<T> &remove ();
+	Node remove ();
+	
+	void set_head (Node *h) {head = h;}
+	Node *get_head () {return head;}
+	
 private:
-	LinkNode<T> *head;
+	Node *head;
 };
 
 
-template <typename T>
-LinkedList<T>::LinkedList () {
+LinkedList::LinkedList () {
 	head = nullptr;
 }
 
 
-template <typename T>
-LinkNode<T> LinkedList<T>::*search (T *match, LinkNode<T> *start) {
-	LinkNode<T> *index = start;
+unsigned int LinkedList::search (const float match, Node *start) {
+	Node *index = start;
 	
-	while (index != nullptr && index->get_value != match->get_value ()) {
+	unsigned int position = 1;
+	while (index != nullptr && index->get_value () != match) {
 		index = index->get_link ();
+		position += 1;
 	}
 	
-	return index;
+	return (index->get_link () == nullptr ? 0 : position);
 }
 
 
-template <typename T>
-void LinkedList<T>::insert_head (LinkNode<T> *node) {
+void LinkedList::insert_head (Node *node) {
 	node->set_link (head);
 	head = node;
 }
 
 
-template <typename T>
-void LinkedList<T>::insert (LinkNode<T> *node, LinkNode<T> *after) {
+void LinkedList::insert (Node *node, Node *after) {
 	node->set_link (after->get_link ());
 	after->set_link (node);
+}
+
+
+const unsigned int menu_select () {
+	unsigned int menu;
+	
+	do {
+		std::cout << "Make a selection:" << std::endl << "1. Add a value (floating-point number)" << std::endl << "2. Search for a value" << std::endl << "3. Find the nth value" << std::endl << "4. Delete the nth value" << std::endl << "5. Display list statistics (count, mean, and median)" << std::endl << "6. Display the list" << std::endl << "7. Quit" << std::endl;
+		std::cin >> menu;
+	} while (menu > 0 && menu < 8);
+	
+	return menu;
 }
 
 
 int main (int argc, char const *argv[]) {
 	std::cout << "Project 2 from Assignment 5" << std::endl << "Linked List" << std::endl << "This program implements a linked list and sorting algorithm." << std::endl << std::endl;
 
-	LinkedList<float> linked_list;
+	LinkedList linked_list;
 	
+	unsigned int menu;
+	do {
+		menu = menu_select ();
+		switch (menu) {
+			case 1:
+			break;
+			
+			case 2:
+			break;
+			
+			case 3:
+			break;
+			
+			case 4:
+			break;
+			
+			case 5:
+			break;
+			
+			case 6:
+			break;
+			
+			default:
+			break;
+		}
+	} while (menu != 7);
 	
 	return 0;
 }
